100a2609eeb01aeabf216fea2f0978c4fbd18ac8
Sailesh Dahal <saileshbro@gmail.com>
2020-01-18 22:47:33 +0545

    -Created a Laravel 6 app -Made resourceful controllers and routers -Made models -Made migrations

---
+++ database/migrations/2020_01_18_154330_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('quantity');
+            $table->unsignedInteger('buyer_id');
+            $table->unsignedInteger('product_id');
+            $table->timestamps();
+            $table->foreign('buyer_id')->references('id')->on('users');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
